2371f618652ef7d8a660ae0c786561095ce5827e
A7med Elna9dy <aelnagdy84@gmail.com>
2023-12-11 00:09:50 +0200

    first comment Create NotesView and Add Theme Create appBare

--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -3,9 +3,35 @@ import 'package:flutter/material.dart';
 class NotesView extends StatelessWidget {
   static const String routeName = "notes_view";
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.05),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+          const SizedBox(width: 10,)
+        ],
+      ),
+      body: Column(),
+    );
   }
 }
