9fa59871c4563059ddf6379090f6e590cd240a6f
A7med Elna9dy <aelnagdy84@gmail.com>
2023-12-10 23:33:34 +0200

    first comment Create NotesView and Add Theme

---
+++ lib/Views/notes_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  static const String routeName = "notes_view";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
